8a05b044fe74222669fb5d7166c9b25e84820c33
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-02-01 17:17:08 -0800

    creates user login

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,3 +1,4 @@
+import Login from '../Login/Login';
 import Register from '../Register/Register';
 import './App.css';
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Register />
+      <Login />
     </div>
   );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="user-login-form" onSubmit={handleSubmit}>
+      <h2>User Login</h2>
+      <input
+        id="email"
+        className="form-input"
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <input
+        id="password"
+        className="form-input"
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button type="submit" className="form-button">
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default Login;
