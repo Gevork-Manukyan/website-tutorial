de58c7879565d4d4c60e7b2fe041f4293cd36191
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-02-01 17:27:37 -0800

    sets up browswer router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,20 @@
+import './App.css';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Register />
-      <Login />
+      <BrowserRouter>
+
+
+        <Routes>
+          <Route path='/login' element={<Login />} ></Route>
+          <Route path='/register' element={<Register />} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
